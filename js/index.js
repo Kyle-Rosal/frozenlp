d889195f1f12f0b5101aeaca74096dee8f7fa7f6
John Kyle Hunter M. Rosal <k.rosal@crh-kyle.local>
2022-09-20 08:37:28 +0800

    Added Menu links

--- js/index.js
+++ js/index.js
@@ -16,49 +16,51 @@
 
 // });
 
-/* -- -- -- Intro read more -- --  -- -- -- > */
-$(document).on('click', '.intro__see-btn',function(){
+// s
+
+$(document).on('click', '.section--intro__see-btn',function(){
     $(this).hide();
     $(this).parent().parent("div").children("div").children(".section__hide").fadeIn(1000);
     console.log($(this).parent().parent("div").children("div").children("div").children('section__hide'));
 })
-/* -- -- -- Story read More -- --  -- -- -- > */
-
-$(document).on('click', '.story__see-btn',function(){
+$(document).on('click', '.section--story__see-btn',function(){
     $(this).hide();
-    $(this).parent().parent("div").children("div").children(".section__hide").fadeIn(100);
+    $(this).parent().parent("div").children("div").children(".section__hide").fadeIn(1000);
     console.log($(this).parent().parent("div").children("div").children("div").children('section__hide'));
 })
-
-/* -- -- -- Character read More -- --  -- -- -- > */
-
-$(document).on('click', '.char__see-btn',function(){
+$(document).on('click', '.section--char__see-btn',function(){
     $(this).hide();
     $(this).parent().parent("div").children(".section__hide").fadeIn(1000);
     console.log($(this).parent().parent("div").children(".section__hide"));
 })
+$(document).on('click', '.staff__info-see-more', function() {
+    $(this).hide();
+    $(this).parent().parent("div").children('.section__hide').fadeIn(1000);
+    console.log($(this).parent().parent("div").children('.section__hide'));  
+})
 
-/* -- -- -- Highlights see more and see less -- --  -- -- -- > */
-
+// -- Blue Icons --
 $(document).on('click', '.hl__info-see-more', function() {
     $(this).hide();
     $(this).parent().children('.hl__info-see-less').fadeIn(1000);
-    $(this).parent().parent("div").children("div").children('.section__hide').fadeIn(1000);
-    console.log($(this).parent().parent("div").children("div").children('.section__hide'));
-    
+    $(this).parent().parent("div").children('.section__hide').fadeIn(1000);
+    console.log($(this).parent().parent("div").children('.section__hide')); 
 })
 $(document).on('click', '.hl__info-see-less',function(){
     $(this).hide();
     $(this).parent().children('.hl__info-see-more').fadeIn(1000);
-    $(this).parent().parent("div").children("div").children('.section__hide').fadeOut(1000)
+    $(this).parent().parent("div").children('.section__hide').fadeOut(1000)
     console.log($(this).parent())
 })
-/* -- -- -- Staff see more and see less -- --  -- -- -- > */
 
-$(document).on('click', '.staff__info-see-more', function() {
-    $(this).hide();
-    $(this).parent().parent("div").parent("div").children(".section__hide").fadeIn();
-    console.log($(this).parent().parent("div").parent("div").children(".section__hide"));  
+// -- Modal --
+$(document).on('click', '.header__menu-btn', function() {
+    $(this).parent().parent("div").parent("div").parent("div").parent("header").children('.header__modal').children('.header__modal-wrapper').show()
+    console.log($(this).parent().parent("div").parent("div").parent("div").parent("header").children('.header__modal').children('.header__modal-wrapper'));      
+})
+$(document).on('click', '.header__modal-close', function() {
+    $(this).parent().parent("div").parent("div").parent("div").parent("div").parent("header").children('.header__modal').children('.header__modal-wrapper').hide()
+    console.log("yowwwww")  
 })
 
 // $(document).on('click', '.sample', function() {
